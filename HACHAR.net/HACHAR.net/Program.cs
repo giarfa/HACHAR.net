b872832867d6e82907a5d8fa67f0898aa918a0e8
Francesco Giarola <giarfa@outlook.com>
2016-05-31 17:50:45 +0200

    Tests

--- HACHAR.net/HACHAR.net/Program.cs
+++ HACHAR.net/HACHAR.net/Program.cs
@@ -14,28 +14,54 @@ namespace HACHAR.net
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[] { 4, 10, 8, 6, 9, 2, 6, 12, 16, 21, 34, 9, 6, 2, 6 };
             Object locker = new Object();
-            string filePath = @"C:\Users\Mehdi Kaabi\Desktop\1.txt";
-            Console.WriteLine("Test_HACHAR_Strings_Partion");
-            Test_HACHAR_Strings_Partion(locker, filePath);
 
-            Console.WriteLine("Test_HACHAR_Strings");
-            Test_HACHAR_Strings(locker, filePath);
+            Console.WriteLine("START\n");
 
-            Console.WriteLine("Test_LinearProbing_Strings");
-            Test_LinearProbing_Strings(locker, filePath);
+            #region Tests with Strings
 
-            Console.WriteLine("DONE");
-            Console.ReadLine();
-        }
-
-        private static void Test_HACHAR_Strings(object locker, string filePath)
-        {
-           
+            string filePath = @"C:\Users\Francesco\Downloads\1.txt";
             //string filePath = @"C:\Users\Francesco\Downloads\Charlie_and_the_Chocolate_Factory-Dahl_Roald.txt";
             string[] AllLines = File.ReadAllLines(filePath);
 
+            Console.WriteLine("Test_HACHAR_Strings_Partion");
+            Test_HACHAR_Strings_Partion(locker, AllLines);
+
+            Console.WriteLine("Test_HACHAR_Strings");
+            Test_HACHAR_Strings(locker, AllLines);
+
+            Console.WriteLine("Test_LinearProbing_Strings");
+            Test_LinearProbing_Strings(locker, AllLines);
+
+            #endregion
+
+            #region Tests with integers
+
+            int max = 1000000;
+            int[] numbers = new int[max];
+            Random rnd = new Random();
+            for (int i = 0; i < max; i++)
+            {
+                numbers[i] = rnd.Next(max);
+            }
+
+            Console.WriteLine("Test_HACHAR_Integers_Partion");
+            Test_HACHAR_Integers_Partion(locker, numbers);
+
+            Console.WriteLine("Test_HACHAR_Integers");
+            Test_HACHAR_Integers(locker, numbers);
+
+            Console.WriteLine("Test_LinearProbing_Integers");
+            Test_LinearProbing_Integers(locker, numbers);
+
+            #endregion
+
+            Console.WriteLine("\nDONE");
+            Console.ReadLine();
+        }
+
+        static void Test_HACHAR_Strings(object locker, string[] AllLines)
+        {
             HACHAR h;
             long sum = 0;
             long partial = 0;
@@ -66,12 +92,8 @@ namespace HACHAR.net
             }
         }
 
-        private static void Test_HACHAR_Strings_Partion(object locker, string filePath)
+        static void Test_HACHAR_Strings_Partion(object locker, string[] AllLines)
         {
-            
-            //string filePath = @"C:\Users\Francesco\Downloads\Charlie_and_the_Chocolate_Factory-Dahl_Roald.txt";
-            string[] AllLines = File.ReadAllLines(filePath);
-
             HACHAR h = new HACHAR(2029, locker);
             OrderablePartitioner<Tuple<int, int>> chunkPart =
              Partitioner.Create(0, AllLines.Length, 2500);
@@ -96,12 +118,8 @@ namespace HACHAR.net
             Console.WriteLine(watch.ElapsedMilliseconds);
         }
 
-        private static void Test_LinearProbing_Strings(object locker, string filePath)
+        static void Test_LinearProbing_Strings(object locker, string[] AllLines)
         {
-            
-            //string filePath = @"C:\Users\Francesco\Downloads\Charlie_and_the_Chocolate_Factory-Dahl_Roald.txt";
-            string[] AllLines = File.ReadAllLines(filePath);
-
             LinearProbing l;
             long sum = 0;
             long partial = 0;
@@ -132,15 +150,100 @@ namespace HACHAR.net
             }
         }
 
+        static void Test_HACHAR_Integers(object locker, int[] numbers)
+        {
+            HACHAR h;
+            long sum = 0;
+            long partial = 0;
+            int size = 110233;
+
+            Console.WriteLine("No Options");
+            for (int i = 0; i < 5; i++)
+            {
+                h = new HACHAR(size, locker);
+                partial = doIt(numbers, h);
+                sum += partial;
+                Console.Write(partial + "; ");
+            }
+            Console.WriteLine("avg: " + (sum / 5));
+
+            for (int i = 1; i <= 8; i++)
+            {
+                sum = 0;
+                Console.WriteLine(i + " cores");
+                for (int j = 0; j < 5; j++)
+                {
+                    h = new HACHAR(size, locker);
+                    partial = doIt(numbers, h, i);
+                    sum += partial;
+                    Console.Write(partial + "; ");
+                }
+                Console.WriteLine("avg: " + (sum / 5));
+            }
+        }
+
+        static void Test_HACHAR_Integers_Partion(object locker, int[] numbers)
+        {
+            HACHAR h = new HACHAR(110233, locker);
+            OrderablePartitioner<Tuple<int, int>> chunkPart =
+             Partitioner.Create(0, numbers.Length, 2000);
+
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            Parallel.ForEach(chunkPart, chunkRange =>
+            {
+
+                for (int i = chunkRange.Item1; i < chunkRange.Item2; i++)
+                {
+                    h.Insert(numbers[i]);
+                }
+            });
+
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+        }
+
+        static void Test_LinearProbing_Integers(object locker, int[] numbers)
+        {
+            LinearProbing l;
+            long sum = 0;
+            long partial = 0;
+            int size = 1028777;
+
+            Console.WriteLine("No Options");
+            for (int i = 0; i < 5; i++)
+            {
+                l = new LinearProbing(size, locker);
+                partial = doIt(numbers, l);
+                sum += partial;
+                Console.Write(partial + "; ");
+            }
+            Console.WriteLine("avg: " + (sum / 5));
+
+            for (int i = 1; i <= 8; i++)
+            {
+                sum = 0;
+                Console.WriteLine(i + " cores");
+                for (int j = 0; j < 5; j++)
+                {
+                    l = new LinearProbing(size, locker);
+                    partial = doIt(numbers, l, i);
+                    sum += partial;
+                    Console.Write(partial + "; ");
+                }
+                Console.WriteLine("avg: " + (sum / 5));
+            }
+        }
+
         static long doIt(string[] lines, HACHAR h)
         {
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-
             Parallel.ForEach(lines, item =>
             {
-                foreach (var s in item.Split(' '))
+                foreach (string s in item.Split(' '))
                 {
                     h.Insert(item);
                 }
@@ -161,10 +264,9 @@ namespace HACHAR.net
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-
             Parallel.ForEach(lines, options, item =>
              {
-                 foreach (var s in item.Split(' '))
+                 foreach (string s in item.Split(' '))
                  {
                      h.Insert(item);
                  }
@@ -180,10 +282,9 @@ namespace HACHAR.net
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-
             Parallel.ForEach(lines, item =>
             {
-                foreach (var s in item.Split(' '))
+                foreach (string s in item.Split(' '))
                 {
                     l.Insert(item);
                 }
@@ -204,13 +305,82 @@ namespace HACHAR.net
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-
             Parallel.ForEach(lines, options, item =>
             {
-                foreach (var s in item.Split(' '))
+                foreach (string s in item.Split(' '))
                 {
                     l.Insert(item);
                 }
+            });
+
+            watch.Stop();
+
+            return watch.ElapsedMilliseconds;
+        }
+
+        static long doIt(int[] numbers, HACHAR h)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            Parallel.ForEach(numbers, item =>
+            {
+                h.Insert(item);
+            });
+
+            watch.Stop();
+
+            return watch.ElapsedMilliseconds;
+        }
+
+        static long doIt(int[] numbers, HACHAR h, int cores)
+        {
+            ParallelOptions options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = cores
+            };
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            Parallel.ForEach(numbers, options, item =>
+            {
+                h.Insert(item);
+            });
+
+            watch.Stop();
+
+            return watch.ElapsedMilliseconds;
+        }
+
+        static long doIt(int[] numbers, LinearProbing l)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            Parallel.ForEach(numbers, item =>
+            {
+                l.Insert(item);
+            });
+
+            watch.Stop();
+
+            return watch.ElapsedMilliseconds;
+        }
+
+        static long doIt(int[] numbers, LinearProbing l, int cores)
+        {
+            ParallelOptions options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = cores
+            };
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            Parallel.ForEach(numbers, options, item =>
+            {
+                l.Insert(item);
             });
 
             watch.Stop();
