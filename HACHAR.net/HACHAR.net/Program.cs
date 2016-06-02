4704a02a519228e95c0f0aba7d37e9124afe3136
Francesco Giarola <giarfa@outlook.com>
2016-06-02 15:29:30 +0200

    Test with spare call

--- HACHAR.net/HACHAR.net/Program.cs
+++ HACHAR.net/HACHAR.net/Program.cs
@@ -24,8 +24,8 @@ namespace HACHAR.net
             //string filePath = @"C:\Users\Francesco\Downloads\Charlie_and_the_Chocolate_Factory-Dahl_Roald.txt";
             string[] AllLines = File.ReadAllLines(filePath);
 
-            Console.WriteLine("Test_HACHAR_Strings_Partion");
-            Test_HACHAR_Strings_Partion(locker, AllLines);
+            //Console.WriteLine("Test_HACHAR_Strings_Partion");
+            //Test_HACHAR_Strings_Partion(locker, AllLines);
 
             Console.WriteLine("Test_HACHAR_Strings");
             Test_HACHAR_Strings(locker, AllLines);
@@ -45,8 +45,8 @@ namespace HACHAR.net
                 numbers[i] = rnd.Next(max);
             }
 
-            Console.WriteLine("Test_HACHAR_Integers_Partion");
-            Test_HACHAR_Integers_Partion(locker, numbers);
+            //Console.WriteLine("Test_HACHAR_Integers_Partion");
+            //Test_HACHAR_Integers_Partion(locker, numbers);
 
             Console.WriteLine("Test_HACHAR_Integers");
             Test_HACHAR_Integers(locker, numbers);
@@ -67,6 +67,12 @@ namespace HACHAR.net
             long partial = 0;
             int size = 2029;
 
+            // Spare call in order to exploit the JIT Compilation
+            Console.WriteLine("Spare call");
+            h = new HACHAR(size, locker);
+            partial = doIt(AllLines, h);
+            Console.WriteLine(partial);
+
             Console.WriteLine("No Options");
             for (int i = 0; i < 5; i++)
             {
@@ -76,6 +82,12 @@ namespace HACHAR.net
                 Console.Write(partial + "; ");
             }
             Console.WriteLine("avg: " + (sum / 5));
+
+            // Spare call in order to exploit the JIT Compilation
+            Console.WriteLine("Spare call");
+            h = new HACHAR(size, locker);
+            partial = doIt(AllLines, h, 1);
+            Console.WriteLine(partial);
 
             for (int i = 1; i <= 8; i++)
             {
@@ -125,6 +137,12 @@ namespace HACHAR.net
             long partial = 0;
             int size = 50551;
 
+            // Spare call in order to exploit the JIT Compilation
+            Console.WriteLine("Spare call");
+            l = new LinearProbing(size, locker);
+            partial = doIt(AllLines, l);
+            Console.WriteLine(partial);
+
             Console.WriteLine("No Options");
             for (int i = 0; i < 5; i++)
             {
@@ -134,6 +152,12 @@ namespace HACHAR.net
                 Console.Write(partial + "; ");
             }
             Console.WriteLine("avg: " + (sum / 5));
+
+            // Spare call in order to exploit the JIT Compilation
+            Console.WriteLine("Spare call");
+            l = new LinearProbing(size, locker);
+            partial = doIt(AllLines, l, 1);
+            Console.WriteLine(partial);
 
             for (int i = 1; i <= 8; i++)
             {
@@ -157,6 +181,12 @@ namespace HACHAR.net
             long partial = 0;
             int size = 110233;
 
+            // Spare call in order to exploit the JIT Compilation
+            Console.WriteLine("Spare call");
+            h = new HACHAR(size, locker);
+            partial = doIt(numbers, h);
+            Console.WriteLine(partial);
+
             Console.WriteLine("No Options");
             for (int i = 0; i < 5; i++)
             {
@@ -166,6 +196,12 @@ namespace HACHAR.net
                 Console.Write(partial + "; ");
             }
             Console.WriteLine("avg: " + (sum / 5));
+
+            // Spare call in order to exploit the JIT Compilation
+            Console.WriteLine("Spare call");
+            h = new HACHAR(size, locker);
+            partial = doIt(numbers, h, 1);
+            Console.WriteLine(partial);
 
             for (int i = 1; i <= 8; i++)
             {
@@ -211,6 +247,12 @@ namespace HACHAR.net
             long partial = 0;
             int size = 1028777;
 
+            // Spare call in order to exploit the JIT Compilation
+            Console.WriteLine("Spare call");
+            l = new LinearProbing(size, locker);
+            partial = doIt(numbers, l);
+            Console.WriteLine(partial);
+
             Console.WriteLine("No Options");
             for (int i = 0; i < 5; i++)
             {
@@ -220,6 +262,12 @@ namespace HACHAR.net
                 Console.Write(partial + "; ");
             }
             Console.WriteLine("avg: " + (sum / 5));
+
+            // Spare call in order to exploit the JIT Compilation
+            Console.WriteLine("Spare call");
+            l = new LinearProbing(size, locker);
+            partial = doIt(numbers, l, 1);
+            Console.WriteLine(partial);
 
             for (int i = 1; i <= 8; i++)
             {
